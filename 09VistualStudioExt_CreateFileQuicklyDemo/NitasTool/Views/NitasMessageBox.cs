8d5f45975343b719c56fea0641335f4fc8e7513a
Nita <41347078+Nita121388@users.noreply.github.com>
2024-12-02 20:09:17 +0800

    fix:【09VS插件CreateFileQuickly】优化弹窗布局

--- 09VistualStudioExt_CreateFileQuicklyDemo/NitasTool/Views/NitasMessageBox.cs
+++ 09VistualStudioExt_CreateFileQuicklyDemo/NitasTool/Views/NitasMessageBox.cs
@@ -9,6 +9,7 @@ namespace NitasTool.Views
         private Button Button1;
         private Button Button2;
         private Button Button3;
+        private TableLayoutPanel tableLayoutPanel;
         private Label Message;
 
         public NitasMessageBox(string title, string message)
@@ -20,72 +21,88 @@ namespace NitasTool.Views
 
         private void InitializeComponent()
         {
-            this.Message = new Label();
-            this.Button1 = new Button();
-            this.Button2 = new Button();
-            this.Button3 = new Button();
-            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
-
+            this.Message = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
             // 
             // Message
             // 
             this.Message.AutoSize = true;
-            this.Message.Font = new Font("宋体", 14F, FontStyle.Bold, GraphicsUnit.Point, 134);
-            this.Message.TextAlign = ContentAlignment.MiddleCenter;
-            this.Message.Dock = DockStyle.Fill;
-
+            this.tableLayoutPanel.SetColumnSpan(this.Message, 3);
+            this.Message.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Message.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Message.Location = new System.Drawing.Point(3, 0);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(680, 100);
+            this.Message.TabIndex = 0;
+            this.Message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Button1
             // 
+            this.Button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.Button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Button1.Location = new System.Drawing.Point(39, 125);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(150, 50);
+            this.Button1.TabIndex = 1;
             this.Button1.Text = "Overwrite";
-            this.Button1.DialogResult = DialogResult.Yes;
-            this.Button1.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
-            this.Button1.Size = new Size(150, 50);
-            this.Button1.Anchor = AnchorStyles.None;
-
             // 
             // Button2
             // 
+            this.Button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button2.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.Button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Button2.Location = new System.Drawing.Point(267, 125);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(150, 50);
+            this.Button2.TabIndex = 2;
             this.Button2.Text = "AutoRename";
-            this.Button2.DialogResult = DialogResult.No;
-            this.Button2.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
-            this.Button2.Size = new Size(150, 50);
-            this.Button2.Anchor = AnchorStyles.None;
-
             // 
             // Button3
             // 
+            this.Button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Button3.Location = new System.Drawing.Point(496, 125);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(150, 50);
+            this.Button3.TabIndex = 3;
             this.Button3.Text = "Cancel";
-            this.Button3.DialogResult = DialogResult.Cancel;
-            this.Button3.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
-            this.Button3.Size = new Size(150, 50);
-            this.Button3.Anchor = AnchorStyles.None;
-
             // 
             // tableLayoutPanel
             // 
-            tableLayoutPanel.ColumnCount = 3;
-            tableLayoutPanel.RowCount = 2;
-            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33F));
-            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33F));
-            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33F));
-            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel.Dock = DockStyle.Fill;
-            tableLayoutPanel.Controls.Add(this.Message, 0, 0);
-            tableLayoutPanel.SetColumnSpan(this.Message, 3);
-            tableLayoutPanel.Controls.Add(this.Button1, 0, 1);
-            tableLayoutPanel.Controls.Add(this.Button2, 1, 1);
-            tableLayoutPanel.Controls.Add(this.Button3, 2, 1);
-
+            this.tableLayoutPanel.ColumnCount = 3;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel.Controls.Add(this.Message, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.Button1, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.Button2, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.Button3, 2, 1);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(686, 200);
+            this.tableLayoutPanel.TabIndex = 0;
             // 
             // NitasMessageBox
             // 
             this.ClientSize = new System.Drawing.Size(686, 200);
-            this.Controls.Add(tableLayoutPanel);
+            this.Controls.Add(this.tableLayoutPanel);
             this.Name = "NitasMessageBox";
-            this.StartPosition = FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
+
         }
     }
 }
